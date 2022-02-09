4b10e87f13e3a76a02b796e6721336b2be930867
wai81 <50743966+wai81@users.noreply.github.com>
2022-02-09 19:08:56 +0300

    added books list and method read all books

--- books.py
+++ books.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+BOOKS = {
+    'book_1': {'title': 'Title One', 'author': 'Author One'},
+    'book_2': {'title': 'Title Two', 'author': 'Author Two'},
+    'book_3': {'title': 'Title Three', 'author': 'Author Three'},
+    'book_4': {'title': 'Title Four', 'author': 'Author Four'},
+    'book_5': {'title': 'Title Five', 'author': 'Author Five'},
+}
+
 @app.get("/")
-async def first_api():
-    return {"message": "Hello Andrei!"}
+async def read_all_books():
+    return BOOKS
