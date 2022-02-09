cdef1ada83ec4917741a7f1baa61d155a0999f78
wai81 <50743966+wai81@users.noreply.github.com>
2022-02-09 22:18:47 +0300

    added enum path parameters

--- books.py
+++ books.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -9,6 +10,12 @@ BOOKS = {
     'book_4': {'title': 'Title Four', 'author': 'Author Four'},
     'book_5': {'title': 'Title Five', 'author': 'Author Five'},
 }
+
+class DirectionName(str, Enum):
+    north = "North"
+    south = "South"
+    east = "East"
+    west = "West"
 
 @app.get("/")
 async def read_all_books():
@@ -21,3 +28,13 @@ async def read_favorite_book():
 @app.get("/books/{book_id}")
 async def read_book(book_id: int):
     return {"book_title": book_id}
+
+@ app.get("/directions/{direction_name}")
+async def get_direction(direction_name: DirectionName):
+    if direction_name == DirectionName.north:
+        return {"Direction": direction_name, "sub": "Up" }
+    if direction_name == DirectionName.south:
+        return {"Direction": direction_name, "sub": "Down" }
+    if direction_name == DirectionName.west:
+        return {"Direction": direction_name, "sub": "Left" }
+    return {"Direction": direction_name, "sub": "Right" }
