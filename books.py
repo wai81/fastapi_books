4ca48981728692cf895fdfabde459295d9d5ddc1
wai81 <50743966+wai81@users.noreply.github.com>
2022-02-10 21:00:51 +0300

    added get and post request

--- books.py
+++ books.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI
 from enum import Enum
 
@@ -17,6 +18,9 @@ class DirectionName(str, Enum):
     east = "East"
     west = "West"
 
+"""
+GET - запросы
+"""
 @app.get("/")
 async def read_all_books():
     return BOOKS
@@ -29,7 +33,8 @@ async def read_favorite_book():
 async def read_book(book_id: int):
     return {"book_title": book_id}
 
-@ app.get("/directions/{direction_name}")
+# путь с параметром
+@app.get("/directions/{direction_name}")
 async def get_direction(direction_name: DirectionName):
     if direction_name == DirectionName.north:
         return {"Direction": direction_name, "sub": "Up" }
@@ -38,3 +43,32 @@ async def get_direction(direction_name: DirectionName):
     if direction_name == DirectionName.west:
         return {"Direction": direction_name, "sub": "Left" }
     return {"Direction": direction_name, "sub": "Right" }
+
+# улучшение параметра пути 
+@app.get("/books/{book_name}")
+async def read_book_by_name(book_name: str):
+    return BOOKS[book_name]
+
+# запросы с параметром
+@app.get("/books/")
+async def read_all_books_by_parametry(skip_book: Optional[str] = None): # str это не обязательный параметор для запроса 
+    if skip_book:
+        new_books = BOOKS.copy()
+        del new_books[skip_book]
+        return new_books
+    return BOOKS
+
+"""
+POST - запросы
+"""
+@app.post("/books/")
+async def create_book(booK_title, book_autor):
+    curent_book_id = 0
+    if len(BOOKS) > 0:
+        for book in BOOKS:
+            x = int(book.split('_')[-1])
+            if x > curent_book_id:
+                curent_book_id = x
+                
+    BOOKS[f'book_{curent_book_id + 1}'] = {'title':  booK_title, 'autor': book_autor}
+    return BOOKS[f'book_{curent_book_id + 1}']
