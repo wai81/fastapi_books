4bc27f6a4c13634b3b2c1999d59aa4ece3eceeda
wai81 <50743966+wai81@users.noreply.github.com>
2022-03-08 20:01:59 +0300

    added method GET by ID Todo from Database

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -20,5 +20,20 @@ def get_db():
 @app.get("/")
 async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Todos).all()
+
+
+@app.get("/todo/{todo_id}")
+async def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos)\
+        .filter(models.Todos.id == todo_id)\
+        .first()
+    if todo_model is not None:
+        return todo_model
+    raise http_exception()
+
+
+"""Функция обработки исключения(ошибок)"""
+def http_exception():
+    return HTTPException(status_code=404, detail="Запись не найдена")
 
 
