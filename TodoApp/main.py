9fdd6f174ff25159a5f98162565e191533e66e74
wai81 <50743966+wai81@users.noreply.github.com>
2022-03-08 15:27:38 +0300

    created main and database connection

---
+++ TodoApp/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import models
+from database import engine
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+
+@app.get("/")
+async def create_database():
+    return {"Database": "Created"}
+
+
