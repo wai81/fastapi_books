0ae916a8ae49a05b0dc69a44657e0459b92064cb
wai81 <50743966+wai81@users.noreply.github.com>
2022-03-08 20:33:51 +0300

    added method POST create Todo from Database

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,20 +1,34 @@
+from typing import Optional
+
 from fastapi import FastAPI, Depends, HTTPException
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
-
 """Сеанс работы с базой данных"""
+
+
 def get_db():
     try:
         db = SessionLocal()
         yield db
     finally:
         db.close()
+
+
+""" 
+Класс для расширения основноймодели, проверка валидации на основе pydantic
+"""
+class Todo(BaseModel):
+    title: str
+    description: Optional[str]
+    priority: int = Field(gt=0, lt=6, description="Приоритет долженбыть между 1-5")
+    complete: bool
 
 
 @app.get("/")
@@ -24,16 +38,31 @@ async def read_all(db: Session = Depends(get_db)):
 
 @app.get("/todo/{todo_id}")
 async def read_todo(todo_id: int, db: Session = Depends(get_db)):
-    todo_model = db.query(models.Todos)\
-        .filter(models.Todos.id == todo_id)\
+    todo_model = db.query(models.Todos) \
+        .filter(models.Todos.id == todo_id) \
         .first()
     if todo_model is not None:
         return todo_model
     raise http_exception()
 
 
+@app.post("/")
+async def create_todo(todo: Todo, db: Session = Depends(get_db)):
+    todo_model = models.Todos()
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.priority = todo.priority
+    todo_model.complete = todo.complete
+
+    db.add(todo_model)
+    db.commit()  # производит запись в базу данных
+
+    return {
+        'status': 201,
+        'transaction': 'Successful'
+    }
+
+
 """Функция обработки исключения(ошибок)"""
 def http_exception():
     return HTTPException(status_code=404, detail="Запись не найдена")
-
-
