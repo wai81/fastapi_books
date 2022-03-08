2d1a6456c25baff0d8be50dd1d48e84a4c28ef53
wai81 <50743966+wai81@users.noreply.github.com>
2022-03-08 19:45:10 +0300

    added method GET read all Todo from Database

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,14 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import models
-from database import engine
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
 
+"""Сеанс работы с базой данных"""
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
 @app.get("/")
-async def create_database():
-    return {"Database": "Created"}
+async def read_all(db: Session = Depends(get_db)):
+    return db.query(models.Todos).all()
 
 
