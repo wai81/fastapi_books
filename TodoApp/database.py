58ffa7aa9e53edd386cd04c6b131126ef293da9a
wai81 <50743966+wai81@users.noreply.github.com>
2022-03-22 22:30:31 +0300

    changed connection database from postgresql

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,12 +2,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+"""Подключение к PostgreSQL"""
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:qwerty@localhost/TodoApplicationsDB"
 
-"""Создание подключения к базе"""
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+     SQLALCHEMY_DATABASE_URL
+ )
+
+# """Подключение к sqlite"""
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+#
+# """Создание подключения к базе"""
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
 """Функция создания сеанса сесии"""
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
