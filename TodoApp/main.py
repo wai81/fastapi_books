9cb624fa65202d92fb58d9bf656ce8fbf8c26777
wai81 <50743966+wai81@users.noreply.github.com>
2022-03-27 22:43:51 +0300

    added template home.html and add-todo.html

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI
 import models
 from database import engine
 from routers import auth, todos
+from starlette.staticfiles import StaticFiles  # added import base static files
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")  # added 'static' directory in application
 
 app.include_router(auth.router)  # добавляем маршрут auth
 app.include_router(todos.router)
--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -40,7 +40,7 @@ class Todo(BaseModel):
 
 @router.get("/test")
 async def test(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    return templates.TemplateResponse("add-todo.html", {"request": request})
 
 
 @router.get("/")
