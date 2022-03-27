0968a2f3846acb261a3471a650fcc21a34225b0d
wai81 <50743966+wai81@users.noreply.github.com>
2022-03-27 22:55:44 +0300

    added template edit-todo.html

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -40,7 +40,7 @@ class Todo(BaseModel):
 
 @router.get("/test")
 async def test(request: Request):
-    return templates.TemplateResponse("add-todo.html", {"request": request})
+    return templates.TemplateResponse("edit-todo.html", {"request": request})
 
 
 @router.get("/")
